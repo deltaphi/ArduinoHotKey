bdfa865f7cadfc25f51832c26c651a94cc14aa20
Damian Philipp <damian.philipp@gmx.net>
2020-04-12 13:49:49 +0200

    Update ArduinoHotKey.ino
    Converted to 4-button sketch with non-identical button mapping.
    Made release delay configurable.

--- Sketch/ArduinoHotKey/ArduinoHotKey.ino
+++ Sketch/ArduinoHotKey/ArduinoHotKey.ino
@@ -4,22 +4,40 @@
 
 #include "Keyboard.h"
 
-#define NUM_BUTTONS (1)
-#define BUTTON_INDEX_OFFSET (2)
+#define NUM_BUTTONS (4)
+#define BUTTON_INDEX_OFFSET (3)
+#define BUTTON_DISTANCE (2)
 
 #define BUTTON_PRESSED(x) (x == LOW)
 
+#define USE_SERIAL_DEBUG (0)
+
 uint8_t buttonState[NUM_BUTTONS];
 
+constexpr buttonIndexToPin(const uint8_t buttonIndex) {
+  return BUTTON_INDEX_OFFSET + (BUTTON_DISTANCE * buttonIndex);
+}
+
 void setup() {
+#if (USE_SERIAL_DEBUG == 1)
+  Serial.begin(9600);
+  Serial.println("ArduinoHotKey ready.");
+#endif
+  
   // Start Keyboard library
   Keyboard.begin();
 
   // Configure all IO pins.
   for (uint8_t i = 0; i < NUM_BUTTONS; ++i) {
     buttonState[i] = HIGH;
-    pinMode(i + BUTTON_INDEX_OFFSET, INPUT);
+    pinMode(buttonIndexToPin(i), INPUT);
   }
+
+}
+
+void releaseAllWithDelay() {
+  delay(100);
+  Keyboard.releaseAll();
 }
 
 /**
@@ -28,6 +46,17 @@ void setup() {
  * \param buttonValue LOW when the button was pressend, HIGH when the button is released.
  */
 void processEdge(uint8_t buttonIndex, uint8_t buttonValue) {
+#if (USE_SERIAL_DEBUG == 1)
+  Serial.print(F("Button "));
+  Serial.print(buttonIndex);
+  Serial.print(F(" was "));
+  if (BUTTON_PRESSED(buttonValue)) {
+    Serial.println(F("pressed."));
+  } else {
+    Serial.println(F("released."));  
+  }  
+#endif
+
   switch (buttonIndex) {
     case 0:
       if (BUTTON_PRESSED(buttonValue)) {
@@ -35,8 +64,32 @@ void processEdge(uint8_t buttonIndex, uint8_t buttonValue) {
         //Keyboard.write('!');
         Keyboard.press(KEY_LEFT_GUI);
         Keyboard.press(KEY_F4);
-        delay(500);
-        Keyboard.releaseAll();
+        releaseAllWithDelay();
+      }
+      break;
+    case 1:
+      if (BUTTON_PRESSED(buttonValue)) {
+        Keyboard.press(KEY_LEFT_SHIFT);
+      } else {
+        Keyboard.release(KEY_LEFT_SHIFT);
+      }
+      break;
+
+    case 2:
+      if (BUTTON_PRESSED(buttonValue)) {
+        Keyboard.press(KEY_LEFT_GUI);
+        Keyboard.press(KEY_LEFT_CTRL);
+        Keyboard.press(KEY_LEFT_ARROW);
+        releaseAllWithDelay();
+      }
+      break;
+      
+    case 3:
+      if (BUTTON_PRESSED(buttonValue)) {
+        Keyboard.press(KEY_LEFT_GUI);
+        Keyboard.press(KEY_LEFT_CTRL);
+        Keyboard.press(KEY_RIGHT_ARROW);
+        releaseAllWithDelay();
       }
       break;
   }
@@ -46,11 +99,12 @@ void loop() {
   // Iterate all configured IO pins.
   for (uint8_t i = 0; i < NUM_BUTTONS; ++i) {
     // Read pin value.
-    uint8_t value = digitalRead(i + BUTTON_INDEX_OFFSET);
+    uint8_t value = digitalRead(buttonIndexToPin(i));
     if (value != buttonState[i]) {
       // On state change, perform an action.
       processEdge(i, value);
       buttonState[i] = value;
     }
   }
+  
 }
